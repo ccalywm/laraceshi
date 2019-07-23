743543a96862a557d3164f2ce891de096cdbd2ff
vagrant <vagrant@homestead>
2019-07-23 03:43:52 +0000

    用户注册表单

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
-
-Route::get('/users', function () {
-    return view('users.index');
-});
-
 Route::get('/','StaticPagesController@index')->name('home');
 Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('/help','StaticPagesController@help')->name('help');
 
 Route::get('signup','UsersController@create')->name('signup');
 
+Route::resource('users','UsersController');
