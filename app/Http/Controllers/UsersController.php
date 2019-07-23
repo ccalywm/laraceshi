e5534df4919eb197efed22a22735a310d29f3685
vagrant <vagrant@homestead>
2019-07-23 07:23:49 +0000

    修改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -38,6 +38,36 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','欢迎注册，快来开始您的旅行吧！');
         return redirect()->route('users.show',[$user]);
+    }
+
+    //返回编辑用户资料页面
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    //更新用户资料
+    public function update(User $user,Request $request)
+    {
+        //验证输入
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        //把输入的内容存到data中
+        $data = [];
+        $data['name'] = $request->name;
+        //若有密码则写入，如果没有则防止写入空置
+        if ($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        //调用update方法进行更新
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+
+        return redirect()->route('users.show',$user);
 
     }
 }
