6bc963509af9d3decb79ed2332c8c35d0b9580f1
vagrant <vagrant@homestead>
2019-07-23 03:53:44 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,9 +11,22 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
-
+    
+    //显示用户个人中心
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+    
+    //处理用户注册表单
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+           'name' => 'required|max:50',
+            'email' => 'required|unique:users|email|max:255',
+            'password' => 'required|confirmed|min:3',
+        ]);
+
+
     }
 }
